56c1f8ec0f66543c47ee5665468ef7d095c02c2b
cedrique <fotsing365@gmail.com>
2017-05-18 13:30:06 +0100

    here I have just add the file on github

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IFCModule
-using to build ifc file with data coming from txt file
+The main objectif of this project is to build an IFC file using IFCSDK and informations coming from txt file. at the moment we just try to modify the the project /example/buildModule from 
+https://github.com/cstb/ifc-sdk/tree/master/examples/buildModel.
---
+++ uuidgenerator.h
@@ -0,0 +1,32 @@
+#ifndef UUIDGenerator_h_h
+#define UUIDGenerator_h_h
+
+#ifdef WIN32
+#	ifdef _MSC_VER
+# 		define _UID_UINT32 unsigned int
+#	elif defined(__GNUC__)
+#		define _UID_UINT32 unsigned int
+#	endif
+#elif defined(sgi)
+        #define _UID_UINT32 uint32_t
+#elif defined(linux)
+# define _UID_UINT32 unsigned int
+#else // Update this to take into account different OS
+# define _UID_UINT32 unsigned int
+#endif
+
+
+#include <string>
+
+class UUIDGenerator
+{
+public:
+        static std::string generateUUID();
+        static std::string generateIfcGloballyUniqueId();
+        static std::string generateEveUniqueId();
+private:
+        UUIDGenerator() {}
+};
+
+
+#endif
